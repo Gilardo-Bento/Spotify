8e3d6fc7563e13a039bbdf3aa1cc0c24fe9e971d
gilardo <gilardoenem@gmail.com>
2025-12-01 10:53:53 -0300

    producao

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,4 @@
-// import { defineConfig } from "vite";
 
-// //export default defineConfig({});
-// export default {
-//   server: {
-//     host: "127.0.0.1",
-//     port: 5173
-//   }
-// };
 
 import { defineConfig } from "vite";
 
